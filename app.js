65cc0a233de210d1211f25fe15409a4efb64d6e4
bala418 <balavtwo@gmail.com>
2022-02-12 11:56:41 +0530

    Remove node modules

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const date = require(__dirname + "/date.js");
 
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -12,49 +10,39 @@ let items = [];
 let workItems = [];
 let day = date.getDate();
 
-
 app.get("/", function (req, res) {
-    res.set('Content-Type', 'text/html');
-    res.render("list", { listTitle: day, newListItems: items });
-    // res.send();
+  res.set("Content-Type", "text/html");
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-
-
-    console.log(req.body);
-    let item = req.body.newItem;
-    console.log(item);
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else {
-
-
-        items.push(item);
-        // res.render("list", { newListItem: item });
-        res.redirect("/");
-    }
+  let item = req.body.newItem;
+  console.log(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    // res.render("list", { newListItem: item });
+    res.redirect("/");
+  }
 });
 
 app.get("/work", function (req, res) {
-    res.render("list", { listTitle: "Work List", newListItems: workItems });
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.post("/work", function (req, res) {
-    let item = req.body.newItem;
-    console.log(item);
-    workItems.push(item);
-    res.redirect("/");
-
-})
+  let item = req.body.newItem;
+  console.log(item);
+  workItems.push(item);
+  res.redirect("/");
+});
 
 app.get("/about", function (req, res) {
-    res.render("about");
-})
-
+  res.render("about");
+});
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
--- date.js
+++ date.js
@@ -1,25 +1,16 @@
-console.log(module);
-
-// module.exports = "Hello World";
-
-//this just exports 1 function
-
-//module.exports or exports does not matter
-
 exports.getDay = function () {
-    let today = new Date();
-    let options = {
-        weekday: "long"
-    };
-    return (today.toLocaleDateString("en-US", options));
-}
+  let today = new Date();
+  let options = {
+    weekday: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+};
 module.exports.getDate = function () {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    return (today.toLocaleDateString("en-US", options));
-}
-console.log(module.exports);
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+};
